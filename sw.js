8f9f5c636849553860ded574e0fb8755d517c8a5
angelmicelti <angelmicelti@users.noreply.github.com>
2026-05-28 09:02:33 +0200

    Revert "Update sw.js"
    This reverts commit bffc66c58cb178855894cf5b2c15cf1472871f76.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ==================== Service Worker - Plan de Actuación Digital ====================
-const CACHE_NAME = 'plan-digital-v26';
+const CACHE_NAME = 'plan-digital-v25';
 const BASE_URL = '/PlanActDigV2/';
 
 const PRECACHE_URLS = [
