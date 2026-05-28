c726d0426196e5e231852e312af56d42b09622f9
angelmicelti <angelmicelti@users.noreply.github.com>
2026-05-28 08:53:21 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ==================== Service Worker - Plan de Actuación Digital ====================
-const CACHE_NAME = 'plan-digital-v23';
+const CACHE_NAME = 'plan-digital-v24';
 const BASE_URL = '/PlanActDigV2/';
 
 const PRECACHE_URLS = [
